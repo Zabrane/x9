acc331047b0c66964f67b08b840b85331bdf6abd
Zabrane <zabrane3@gmail.com>
2023-07-16 00:14:19 +0200

    Update compile_profiler.sh

--- profiling/compile_profiler.sh
+++ profiling/compile_profiler.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-gcc -Wextra -Wall -Werror -pedantic -flto -O3 -march=native x9_profiler.c ../x9.c -o X9_PROF
+gcc -Wextra -Wall -Werror -pedantic -flto -O3 -pthread -lpthread -march=native x9_profiler.c ../x9.c -o X9_PROF
